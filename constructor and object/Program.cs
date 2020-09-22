2d79a051255b66312290e1c8008cd21b306fc3d4
rohit maharjan <rmaharjan61@gmail.com>
2020-09-22 12:25:29 +0545

    after making RohitGF class

---
+++ constructor and object/Program.cs
@@ -0,0 +1,163 @@
+// ecample of COnstructor and object
+
+using System;
+					
+public class Program
+{
+	public class RohitGf
+	{
+	public string name ="";
+	public string hometown ="";
+	public int age;
+		
+		static string HeroName = "Rohit Maharjan";
+		
+		/*public RohitGf (string gfname, string gfhometown, int gfage)   // Constructor
+		{
+			name = gfname;
+			hometown = gfhometown;
+			age = gfage;
+		}*/
+		
+		/* public int addRohitImaginaryGf()							// method not needed at all
+		{
+			
+		}*/
+	}
+	
+	
+	
+	public static void Main()
+	{
+		//RohitGf sonam = new RohitGf("Sonam Kunwar","Nepaljung",22);   // constuctor
+		
+		RohitGf kali = new RohitGf();									// normal object
+		kali.name = "kali kunwar";
+		kali.hometown = "ktm";
+		kali.age = 20;
+		
+		Console.WriteLine(kali.name + " " + kali.hometown + " " + kali.age);
+		
+		//Console.WriteLine(sonam.name+ " "+ sonam.hometown+" "+ sonam.age);
+		
+		
+	}
+}
+
+//  ------------- Using Method --------------
+using System;
+					
+public class Program
+{
+	public class RohitGf
+	{
+	public string name ="";
+	public string hometown ="";
+	public int age;
+		
+		static string HeroName = "Rohit Maharjan";
+		
+		/*public RohitGf (string gfname, string gfhometown, int gfage)
+		{
+			name = gfname;
+			hometown = gfhometown;
+			age = gfage;
+		}*/
+		
+		 public void addRohitImaginaryGf(string gfname, string gfhometown, int gfage)
+		{
+			name = gfname;
+			hometown = gfhometown;
+			age = gfage;
+		}
+	}
+	
+	
+	
+	public static void Main()
+	{
+		//RohitGf sonam = new RohitGf("Sonam Kunwar","Nepaljung",22);
+		
+		RohitGf nanu = new RohitGf();
+		nanu.addRohitImaginaryGf("nanu kunwar","mumbai",18);
+		
+		/*
+		RohitGf kali = new RohitGf();
+		kali.name = "kali kunwar";
+		kali.hometown = "ktm";
+		kali.age = 20;
+		*/
+		
+		//Console.WriteLine(kali.name + " " + kali.hometown + " " + kali.age);
+		
+		//Console.WriteLine(sonam.name+ " "+ sonam.hometown+" "+ sonam.age);
+		
+		Console.WriteLine(nanu.name + " " + nanu.hometown + " " + nanu.age);
+	}
+}
+
+
+
+//         ------------ menthod Functiom overloading ----------
+using System;
+					
+public class Program
+{
+	public class RohitGf
+	{
+	public string name ="";
+	public string hometown ="";
+	public int age;
+		
+		static string HeroName = "Rohit Maharjan";
+		
+		/*public RohitGf (string gfname, string gfhometown, int gfage)
+		{
+			name = gfname;
+			hometown = gfhometown;
+			age = gfage;
+		}*/
+		
+		 public void addRohitImaginaryGf(string gfname, string gfhometown, int gfage)
+		{
+			name = gfname;
+			hometown = gfhometown;
+			age = gfage;
+		}
+		
+		public void addRohitImaginaryGf(string gfname, int gfage)
+		{
+			name = gfname;
+			age = gfage;
+		}
+		
+	}
+	
+	
+	
+	public static void Main()
+	{
+		//RohitGf sonam = new RohitGf("Sonam Kunwar","Nepaljung",22);
+		
+		/*RohitGf nanu = new RohitGf();
+		nanu.addRohitImaginaryGf("nanu kunwar","mumbai",18);*/
+		
+		RohitGf funOverloading = new RohitGf();
+		funOverloading.addRohitImaginaryGf("function overloading kunwar",30);
+		
+		/*
+		RohitGf kali = new RohitGf();
+		kali.name = "kali kunwar";
+		kali.hometown = "ktm";
+		kali.age = 20;
+		*/
+		
+		//Console.WriteLine(kali.name + " " + kali.hometown + " " + kali.age);
+		
+		//Console.WriteLine(sonam.name+ " "+ sonam.hometown+" "+ sonam.age);
+		
+		//Console.WriteLine(nanu.name + " " + nanu.hometown + " " + nanu.age);
+		
+		Console.WriteLine(funOverloading.name + " " + funOverloading.age);
+	}
+}
